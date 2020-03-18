a46c6d2964e242d4c38d77e7650d2777bef3335a
vikas mishra <vikasmishra227@gmail.com>
2020-03-19 00:08:23 +0530

    Update fetchContentful.js

--- assets/js/fetchContentful.js
+++ assets/js/fetchContentful.js
@@ -1,17 +1,14 @@
 
 async function getData() {
     const client = await contentful.createClient({
-        space: "aq57ffyg8jbb",
-        accessToken: "5DXMbsR5nN-cmkrElpnr8wl_CZB6soqIdzTsIBbz7nc"
+        space: "4lb9qnfm7q3h",
+		accessToken: "THkiXl7sEQYJU2g1ktl_fv-sV05DIr_nZbpwP_7aUo4"
     });
-    return await client.getEntry("5qtrwY6fGvxL7VVJFNjwEO");
+    return await client.getEntry("4WN4ltr3UwdUadiVGAlN1T");
 }
 
 getData().then(entry => {
-    document.getElementsByClassName('img-fluid')[0].src = 'https:' + entry.fields.images.fields.file.url;
-    document.getElementsByClassName('fs-0')[0].innerHTML = entry.fields.articleTitle;
-    const rawRichTextField = entry.fields.description;
-    return documentToHtmlString(rawRichTextField);
-}).then(renderedHtml => {
-    document.getElementsByClassName('mb-0')[2].innerHTML = renderedHtml;
+    document.getElementsByClassName('img-fluid')[0].src = 'https:' + entry.fields.image.fields.file.url;
+	document.getElementsByClassName('fs-0')[0].innerHTML = entry.fields.title;
+    document.getElementsByClassName('mb-0')[2].innerHTML =  entry.fields.description;
 }).catch(err => console.log(err))
